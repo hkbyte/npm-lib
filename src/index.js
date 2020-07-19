b305f29fd5b4662c82508f766f4aa1bb0808792f
Kunal Gosrani <iamkunalgosrani@gmail.com>
2020-07-19 19:36:35 +0530

    package.json file updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { Schema } = require("./validations")
 
 module.exports = {
+    joi: require("@hapi/joi"),
     types: require("./types"),
     Schema: Schema,
     error: require("./errors")
